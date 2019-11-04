bb9f113a878c20dc89eed8706e32e4b1ebd56f53
Monica Djunaidi <modjun12@gmail.com>
2019-11-03 18:34:18 -0600

    wip

--- src/javascripts/components/singleBoard/singleBoard.js
+++ src/javascripts/components/singleBoard/singleBoard.js
@@ -1,7 +1,12 @@
 import './singleBoard.scss';
+import boardsData from '../../helpers/data/boardsData';
 
 const buildSingleBoard = () => {
-
+  boardsData.getBoard()
+    .then((boards) => {
+      console.log('it worked', boards);
+    })
+    .catch((error) => console.error(error));
 };
 
 export default { buildSingleBoard };

304270801e2d591b4223baea7b929fad9d4a8a46
Monica Djunaidi <modjun12@gmail.com>
2019-11-05 21:35:58 -0600

    boards printed

--- src/javascripts/components/allBoards/allBoards.js
+++ src/javascripts/components/allBoards/allBoards.js
@@ -1,7 +1,7 @@
 import './allBoards.scss';
 // import smash from '../../helpers/data/smash';
-// import utilities from '../../helpers/utilities';
-// import pins from '../pins/pins';
+import utilities from '../../helpers/utilities';
+import pins from '../pins/pins';
 import boardsData from '../../helpers/data/boardsData';
 // import pinsData from '../../helpers/data/pinsData';
 
@@ -23,6 +23,12 @@ const buildAllBoard = (uid) => {
   boardsData.getBoardByUid(uid)
     .then((boards) => {
       console.log('it worked', boards);
+      let domString = '<div id="boardSection" class="d-flex flex-wrap">';
+      boards.forEach((board) => {
+        domString += pins.makeAPin(board);
+      });
+      domString += '</div>';
+      utilities.printToDOM('boards', domString);
     })
     .catch((error) => console.error(error));
 };
--- src/javascripts/components/pins/pins.js
+++ src/javascripts/components/pins/pins.js
@@ -1,9 +1,9 @@
 const makeAPin = (board) => {
   let domString = '';
-  if (board.board.id) {
+  if (board.id) {
     domString += `
     <div class="card col-4">
-      <img src=${board.imageURL} class="card-img-top" alt="...">
+      <p>${board.description}</p>
     </div>
     `;
   }
